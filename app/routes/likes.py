876364f18c09ad00e3870a6dd5b456414e6d3071
asma48 <asmakaramat48@gmail.com>
2025-07-17 17:22:52 +0500

    like updated

--- app/routes/likes.py
+++ app/routes/likes.py
@@ -43,25 +43,20 @@ def likes(post_id: int,  db: db_session, current_user: Annotated[dict, Depends(v
 
 @likes_router.get("/{post_id}")
 def total_like(post_id: int, db: db_session, current_user: Annotated[dict, Depends(verify_token)]):    
-    try: 
-        db_likes = db.query(Likes).filter(Likes.post_id == post_id).count()
-        if db_likes is None:
-            return JSONResponse(content={
-        "message": "No likes on this Post",
-        "status":404}, status_code=status.HTTP_404_NOT_FOUND)
 
-        db_user_likes = db.query(Likes).filter(Likes.post_id == post_id).all()
-        users = []
-        for user in db_user_likes:
-            users.append({"user_id": user.user_id})
-
+    db_likes = db.query(Likes).filter(Likes.post_id == post_id).count()
+    if db_likes is None:
         return JSONResponse(content={
+    "message": "No likes on this Post",
+    "status":404}, status_code=status.HTTP_404_NOT_FOUND)
+
+    db_user_likes = db.query(Likes).filter(Likes.post_id == post_id).all()
+    users = []
+    for user in db_user_likes:
+        users.append({"user_id": user.user_id})
+
+    return JSONResponse(content={
             "message": "Total Likes",
             "data": {"likes": db_likes,
                     "users" : users},  
             "status":200}, status_code=status.HTTP_200_OK)
-   
-    except:
-        return JSONResponse(content={
-        "message": "Error",
-        "status":404}, status_code=status.HTTP_404_NOT_FOUND)
